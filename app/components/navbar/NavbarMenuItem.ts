ae50529160569164310c8cd9e33fc1e5718c2fbc
yjgaia <yjdev88@gmail.com>
2024-10-27 20:40:58 +0900

    Refactor NavbarMenuItem to clone icons when setting active or inactive state

--- app/components/navbar/NavbarMenuItem.ts
+++ app/components/navbar/NavbarMenuItem.ts
@@ -22,12 +22,12 @@ export default class NavbarMenuItem extends DomNode<HTMLAnchorElement> {
   }
 
   public active() {
-    this.iconContainer.empty().append(this.iconFilled);
+    this.iconContainer.empty().append(this.iconFilled.clone());
     this.addClass("active");
   }
 
   public inactive() {
-    this.iconContainer.empty().append(this.icon);
+    this.iconContainer.empty().append(this.icon.clone());
     this.removeClass("active");
   }
 }
