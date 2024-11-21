701b9693cce9469960cd2a2802c17cc14e3b87f4
yjgaia <yjdev88@gmail.com>
2024-11-21 22:36:37 +0900

    Add Gaia UI preset and buffer dependency; enhance AppConfig for user profile handling

--- app/AppConfig.ts
+++ app/AppConfig.ts
@@ -1,5 +1,12 @@
 import { Router } from "@common-module/app";
+import {
+  DropdownMenuGroup,
+  DropdownMenuItem,
+} from "@common-module/app-components";
+import { SocialCompConfig } from "@common-module/social-components";
 import { WalletLoginConfig } from "@common-module/wallet-login";
+import { ProfileIcon } from "@gaiaprotocol/svg-icons";
+import { GaiaUIPreset } from "@gaiaprotocol/ui-preset";
 import { base, baseSepolia } from "@wagmi/core/chains";
 import { GaiaProtocolConfig } from "gaiaprotocol";
 
@@ -14,6 +21,7 @@ class AppConfig implements IAppConfig {
 
   public init(config: IAppConfig) {
     Object.assign(this, config);
+    GaiaUIPreset.init();
 
     GaiaProtocolConfig.onLoggedInUserPersonaNotFound = () =>
       Router.go("/onboarding");
@@ -22,6 +30,31 @@ class AppConfig implements IAppConfig {
       config.isDevMode,
       config.isTestnet,
     );
+
+    SocialCompConfig.goLoggedInUserProfile = async (user) => {
+      Router.go(
+        user.isFallback
+          ? "/onboarding"
+          : `/${user.name.endsWith(".gaia") ? user.name : user.id}`,
+      );
+    };
+
+    SocialCompConfig.getLoggedInUserMenu = async (menu, user) => {
+      return new DropdownMenuGroup(
+        new DropdownMenuItem({
+          icon: new ProfileIcon(),
+          label: "My Persona",
+          onClick: () => {
+            Router.go(
+              user.isFallback
+                ? "/onboarding"
+                : `/${user.name.endsWith(".gaia") ? user.name : user.id}`,
+            );
+            menu.remove();
+          },
+        }),
+      );
+    };
 
     WalletLoginConfig.init({
       chains: [base, baseSepolia] as any,
