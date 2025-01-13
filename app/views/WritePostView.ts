ac32a80d7729ed8798080794b8d9c2a22547cde4
yjgaia <yjdev88@gmail.com>
2025-01-13 13:51:08 +0900

    Enhance WritePostView with new layout and post functionality, including form integration and styling updates

--- app/views/WritePostView.ts
+++ app/views/WritePostView.ts
@@ -1,14 +1,34 @@
-import { el, View } from "@common-module/app";
+import { el, Router, View } from "@common-module/app";
+import { Button, ButtonType } from "@common-module/app-components";
 import { PostForm } from "@common-module/social-components";
+import { WalletLoginManager } from "@common-module/wallet-login";
+import { PersonaPostRepository } from "gaiaprotocol";
 import Layout from "./Layout.js";
 
 export default class WritePostView extends View {
+  private form: PostForm;
+
   constructor() {
     super();
     Layout.content = this.container = el(
       ".write-post-view",
       el("header", el("h1", "Write a post")),
-      new PostForm(),
+      el("main", this.form = new PostForm()),
+      el(
+        "footer",
+        new Button(".post", {
+          type: ButtonType.Contained,
+          title: "Post",
+          onClick: () => this.savePost(),
+        }),
+      ),
     );
+
+    if (!WalletLoginManager.isLoggedIn()) Router.goWithoutHistory("/");
+  }
+
+  private async savePost(): Promise<void> {
+    const data = this.form.getData();
+    await PersonaPostRepository.writePost(data.title, data.content);
   }
 }
