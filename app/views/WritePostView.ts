d5658b83a2bfdebd3af097257cc3617b9d093133
yjgaia <yjdev88@gmail.com>
2025-01-13 13:39:18 +0900

    Add new styles for various views and components, including WritePostView, persona view, and navbar, while removing unused public styles

--- app/views/WritePostView.ts
+++ app/views/WritePostView.ts
@@ -1,9 +1,14 @@
 import { el, View } from "@common-module/app";
+import { PostForm } from "@common-module/social-components";
 import Layout from "./Layout.js";
 
 export default class WritePostView extends View {
   constructor() {
     super();
-    Layout.content = this.container = el(".write-post-view", "Write a post");
+    Layout.content = this.container = el(
+      ".write-post-view",
+      el("header", el("h1", "Write a post")),
+      new PostForm(),
+    );
   }
 }
--- webpack.config.testnet.ts
+++ webpack.config.testnet.ts
@@ -5,7 +5,7 @@ import webpack from "webpack";
 const config: webpack.Configuration = {
   entry: {
     "bundle": "./app/testnet.ts",
-    __less: "./public/style/main.less",
+    __less: "./style/main.less",
   },
   module: {
     rules: [
