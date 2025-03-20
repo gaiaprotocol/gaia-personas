0b4274b0bcaf34d39ab728642103dc3b2720ce8b
yjgaia <yjdev88@gmail.com>
2025-03-20 13:27:13 +0900

    Update index.ts to fetch base HTML for /test path and append access key to script tag

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ export default {
 
 		if (url.pathname === "/test") {
 			const accessKey = url.searchParams.get("access_key");
-			let html = await (await env.ASSETS.fetch("/index.html")).text();
+			let html = await (await env.ASSETS.fetch("/")).text();
 			html = html.replace(
 				'<script src="/bundle.js" onerror="handleScriptError()"></script>',
 				`<script src="/bundle.js?access_key=${accessKey}" onerror="handleScriptError()"></script>`,
